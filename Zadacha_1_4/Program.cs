e91c62772f4359dc34d1689b050f7df9ecb03d27
Yuliya <bredis20@bk.ru>
2022-09-09 23:59:36 +0300

    Added Zadacha_1_4

---
+++ Zadacha_1_4/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+Console.WriteLine("Найдите максимальное число из трёх");
+ string[] i = Console.ReadLine().Split(' ');
+            int num1 = Convert.ToInt32(i[0]);
+            int num2 = Convert.ToInt32(i[1]);
+            int num3 = Convert.ToInt32(i[2]);
+            Console.WriteLine(Math.Max(num1, Math.Max(num2, num3)));
